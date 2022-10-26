0001682e1d0530ed7d6eda3655a26f9308c89110
Junho Moon <92602653+momomomoon@users.noreply.github.com>
2022-10-27 02:50:36 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    puts("|\\_/|");
+    puts("|q p|   /}");
+    puts("\( 0 \)\"\"\"\\");
+    puts("|\"^\"`    |");
+    puts("||_/=\\\\__|");
+
+    return 0;;
+}
