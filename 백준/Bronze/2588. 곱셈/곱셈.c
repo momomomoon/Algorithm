a69feea1cc566081e98111b6affe9679755f8b14
Junho Moon <92602653+momomomoon@users.noreply.github.com>
2022-11-01 12:08:56 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.c
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main(){
-    int num1, num2, sum1, sum2, sum3;
-    scanf("%d%d",&num1, &num2); 
+    int num1, num2;
+    scanf("%d %d", &num1, &num2); 
     
-    printf("%d\n", sum1 = num1*(num2%10));
-    printf("%d\n", sum2 = num1*(num2%100/10));
-    printf("%d\n", sum3 = num1*(num2%1000/100));
+    printf("%d\n", num1 * (num2 % 10));
+    printf("%d\n", num1 * (num2 % 100 / 10));
+    printf("%d\n", num1 * (num2 % 1000 / 100));
 
-    printf("%d\n", sum1 + (sum2 * 10) + (sum3 * 100));
+    printf("%d\n", num1 * num2);
     
     return 0;
 }
