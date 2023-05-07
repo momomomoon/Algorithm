51150e4adfed45406f3c3e492d4f70bc615a1577
Junho Moon <92602653+momomomoon@users.noreply.github.com>
2023-05-07 18:39:43 +0900

    Update and rename A－B.c to A－B.swift

--- 백준/Bronze/1001. A－B/A－B.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int x, b;
-    scanf("%d%d", &x, &b);
-    printf("%d\n", x-b);
-    
-    return 0;
-}
---
+++ 백준/Bronze/1001. A－B/A－B.swift
@@ -0,0 +1,2 @@
+let num = readLine()!.split(separator: " ").map {Int($0)!}
+print(num[0] - num[1])
