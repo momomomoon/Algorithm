5fe0ae1e73d5de37fb0a31713510a81aaef0ab69
Junho Moon <92602653+momomomoon@users.noreply.github.com>
2024-09-02 20:45:15 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.39 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    
+    for(int i = 0; i < numbers.size(); i++)
+    {
+        numbers[i] = numbers[i] * 2;
+    }
+    
+    answer = numbers;
+    return answer;
+}
